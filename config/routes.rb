07326808c803bb1c27503b96498efede9489cc71
ectachyon <vandusseldorptravis@gmail.com>
2015-01-21 09:40:20 -0600

    Implement authentication
    Generate user model/controller and sessions controller to allow a user
    to log in.
    Still need to add destroy action/ log out function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Blog::Application.routes.draw do
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root "users#new"
+  resources :users
   resources :posts
+  resources :sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
